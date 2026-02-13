de7dc0f7c64a310d30199482c4e67277fad293bf
Kiet Bui <kietbuianh@gmail.com>
2026-02-13 01:01:53 -0800

    Added timelapse and individual images option.

--- yoloDetModel.py
+++ yoloDetModel.py
@@ -7,6 +7,10 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
+import re
+import os
+from pathlib import Path
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -52,6 +56,16 @@ def parse_args() -> argparse.Namespace:
         default=60,
         help="Polling interval for URL mode (seconds)",
     )
+    parser.add_argument(
+        "--timelapse",
+        type=str,
+        help="Run inference on TripCheck camera URL everytime it updates and saves the result",
+    )
+    parser.add_argument(
+        "--test",
+        type=str,
+        help="Folder path to test"
+    )
     return parser.parse_args()
 
 
@@ -88,12 +102,16 @@ def main():
     print(f"[INFO] Loading YOLO-12 model from: {args.weights}")
     model = YOLO(args.weights)
 
-    window_name = "YOLO-12 Object Detection (press 'q' to quit)"
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    if args.test:
+        test()
 
-    if args.url:
+    elif args.url:
         print(f"[INFO] Running in TripCheck mode")
         print(f"[INFO] URL: {args.url}")
+
+        window_name = "YOLO-12 Object Detection (press 'q' to quit)"
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
         last_hash = None
         while True:
             try:
@@ -122,8 +140,10 @@ def main():
 
             time.sleep(args.interval)
 
-    else:
+    elif args.timelapse:
+        time_lapse()
 
+    else:
         print(f"[INFO] Opening camera index {args.camera}")
         cap = open_camera(args.camera)
 
@@ -171,6 +191,91 @@ def main():
             cap.release()
             cv2.destroyAllWindows()
             print("[INFO] Camera and windows closed, exiting.")
+
+
+def time_lapse():
+    args = parse_args()
+
+    print(f"[INFO] Loading YOLO-12 model from: {args.weights}")
+    model = YOLO(args.weights)
+
+    # window_name = "YOLO-12 Object Detection (press 'q' to quit)"
+    # cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+    if args.timelapse:
+        print(f"[INFO] Running in TripCheck mode with timelapse")
+        print(f"[INFO] Path: {args.timelapse}")
+
+        urls = []
+
+        with open(args.timelapse) as file:
+            urls = file.readlines()
+
+        hash_dict = {url : None for url in urls}
+
+
+        while True:
+            try:
+                for url in urls:
+                    frame, h = fetch_tripcheck_image(url)
+
+                    folder_name = re.sub(r".*cams/|_.*|\n", "", url)
+                    folder_name = re.sub(r"%20", " ", folder_name)
+
+                    if not os.path.isdir("./timelapse/"+folder_name):
+                        os.makedirs("./timelapse/"+folder_name)
+
+                    if frame is None:
+                        print("[WARN] Failed to decode image")
+                        time.sleep(20)
+                        continue
+                    
+                    if h == hash_dict[url]:
+                        print("[INFO] Image unchanged, skipping")
+                        time.sleep(20)
+                        continue
+                        
+                    hash_dict[url] = h
+                    results = model(frame, imgsz=args.imgsz, conf=args.conf, verbose=False,)
+                    annotated = results[0].plot(labels=True, conf=True)
+
+                    t = time.localtime()
+                    time_dir = f"{t.tm_mon}-{t.tm_mday}_{t.tm_hour}-{t.tm_min}"
+                    save_location = f"./timelapse/{folder_name}/{time_dir}.jpg"
+                    cv2.imwrite(save_location, annotated)
+
+
+            except Exception as e:
+                print(e)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+            time.sleep(150)
+
+
+def test():
+    args = parse_args()
+
+    input_path = Path(args.test)
+    print(input_path)
+    output_path = Path(r"C:\Users\fiery\Desktop\Ares\School\Capstone\Count-How-Many\results")
+
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    model = YOLO(args.weights)
+    image_files = list(input_path.glob("*.png"))
+    print(image_files)
+
+    for image in image_files:
+        result = model.predict(
+            source=str(image),
+            save=True,
+            project=output_path,
+            name="predictions",
+            exist_ok=True
+        )
+        
 
 
 if __name__ == "__main__":
